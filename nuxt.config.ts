b9085ca4ba1c612f9aa3fb0b1a19e11763013e38
@samanfattahi <samanft752@gmail.com>
2024-09-15 18:31:18 +0330

    working on alias

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,25 +1,29 @@
 import {resolve} from "pathe";
+import { fileURLToPath } from 'url'
+import { dirname, join } from 'path'
+
+const currentDir = dirname(fileURLToPath(import.meta.url))
 
 export default defineNuxtConfig({
-  alias: {
-    '#spinner': resolve('./')
-  },
-  components: [
-    {
-      // can be relative, alias or absolute
-      path: 'components',
-
-      // components organised in sub-folders should not be prefixed
-      pathPrefix: false,
-
-      // ensure contact form is accessible in Nuxt content
-      // the alternative would be to rename it ContactForm.global.vue
-      global: true
+    alias: {
+        '#spinner': resolve(currentDir)
     },
-  ],
+    components: [
+        {
+            // can be relative, alias or absolute
+            path: 'components',
 
-  dirs: {
-    // move public folder here
-    public: 'public'
-  }
+            // components organised in sub-folders should not be prefixed
+            pathPrefix: false,
+
+            // ensure contact form is accessible in Nuxt content
+            // the alternative would be to rename it ContactForm.global.vue
+            global: true
+        },
+    ],
+
+    dirs: {
+        // move public folder here
+        public: 'public'
+    }
 })
